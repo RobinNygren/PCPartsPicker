5b42eb0de9b66dd2ff9dd9bdac42eeb0df1015f0
Robin <robin_nygren@outlook.com>
2024-04-04 14:24:56 +0200

    added chassitype

---
+++ src/PartTypes.ts
@@ -0,0 +1,14 @@
+export type Chassis = {
+  id: string;
+  brand: string;
+  model: string;
+  formFactor: string;
+  material: string;
+  type: string;
+  price: number;
+  img: string;
+};
+
+export type ChassisData = {
+  chassis: Chassis[];
+};
